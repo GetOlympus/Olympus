ec814b35e159238aba3abe13cef260cdf578b8eb
Achraf Chouk <achrafchouk@gmail.com>
2021-08-12 10:46:43 +0200

    make Olympus fully compatible with Multisite WordPress mode

--- web/statics/mu-plugins/autoload.php
+++ web/statics/mu-plugins/autoload.php
@@ -25,16 +25,6 @@ use Olympus\Components\Autoloader\MuPlugins;
  */
 
 /**
- * Update theme root folder.
- */
-add_filter('theme_root', function () {
-    return WP_THEME_DIR;
-});
-add_filter('theme_root_uri', function () {
-    return WP_THEME_URL;
-});
-
-/**
  * Autoload all mu-plugins.
  * As the first autoloaded file, all useful constants are defined here.
  */
@@ -106,4 +96,96 @@ add_action('setup_theme', function () {
      */
 
     (new MuPlugins(OL_ISADMIN))->init();
+});
+
+/**
+ * Update theme root folder.
+ */
+add_filter('theme_root', function () {
+    return WP_THEME_DIR;
+});
+add_filter('theme_root_uri', function () {
+    return WP_THEME_URL;
+});
+
+/**
+ * Update network admin URL.
+ */
+add_filter('network_site_url', function ($url, $path, $scheme) {
+    // Check network admin path
+    if ('wp-admin/network/' !== $path || 'admin' !== $scheme) {
+        return $url;
+    }
+
+    // Fix path by adding the WORDPRESSDIR constant
+    $current_network = get_network();
+    $url = set_url_scheme('http://'.$current_network->domain.'/'.WORDPRESSDIR.$current_network->path, $scheme);
+
+    return $url.ltrim($path, '/');
+}, 10, 3);
+
+/**
+ * Update automatically the `own.php` configuration file.
+ */
+add_action('admin_footer', function () {
+    $screen = get_current_screen();
+
+    // Check network admin page or POST action
+    if ('network' !== $screen->base || !$_POST) {
+        return;
+    }
+
+    // Set target file and contents
+    $target   = APPPATH.'config'.S.'own.php';
+    $contents = '';
+
+    // Check target file
+    if (!file_exists($target)) {
+        $contents = "<?php\n\n/**\n * File auto-generated.\n */\n\n";
+    } else {
+        $contents = file_get_contents($target);
+        $contents .= "\n/**\n * Contents auto-generated for multisite.\n */\n\n";
+    }
+
+    // Check `define('MULTISITE', true);` constant definition
+    if (!preg_match_all('#^\s?define\(\s?\'MULTISITE\'\s?,#msi', $contents, $matches)) {
+        $contents .= "define('MULTISITE', true);\n";
+    }
+
+    // Check `define('SUBDOMAIN_INSTALL', ***);` constant definition
+    if (!preg_match_all('#^\s?define\(\s?\'SUBDOMAIN_INSTALL\'\s?,#msi', $contents, $matches)) {
+        global $wpdb;
+
+        // Get value from DB
+        $subdomain_install = (bool) $wpdb->get_var('
+            SELECT meta_value FROM '.$wpdb->sitemeta.' WHERE site_id = 1 AND meta_key = "subdomain_install"
+        ');
+
+        $contents .= "define('SUBDOMAIN_INSTALL', ".($subdomain_install ? 'true' : 'false').");\n";
+    }
+
+    // Check `define('DOMAIN_CURRENT_SITE', ***);` constant definition
+    if (!preg_match_all('#^\s?define\(\s?\'DOMAIN_CURRENT_SITE\'\s?,#msi', $contents, $matches)) {
+        $domain_current_site = '\''.get_clean_basedomain().'\'';
+        $contents           .= "define('DOMAIN_CURRENT_SITE', $domain_current_site);\n";
+    }
+
+    // Check `define('PATH_CURRENT_SITE', ***);` constant definition
+    if (!preg_match_all('#^\s?define\(\s?\'PATH_CURRENT_SITE\'\s?,#msi', $contents, $matches)) {
+        $path_current_site = '\''.parse_url(trailingslashit(get_option('home')), PHP_URL_PATH).'\'';
+        $contents         .= "define('PATH_CURRENT_SITE', $path_current_site);\n";
+    }
+
+    // Check `define('SITE_ID_CURRENT_SITE', ***);` constant definition
+    if (!preg_match_all('#^\s?define\(\s?\'SITE_ID_CURRENT_SITE\'\s?,#msi', $contents, $matches)) {
+        $contents .= "define('SITE_ID_CURRENT_SITE', 1);\n";
+    }
+
+    // Check `define('BLOG_ID_CURRENT_SITE', ***);` constant definition
+    if (!preg_match_all('#^\s?define\(\s?\'BLOG_ID_CURRENT_SITE\'\s?,#msi', $contents, $matches)) {
+        $contents .= "define('BLOG_ID_CURRENT_SITE', 1);\n";
+    }
+
+    // Puts contents into target configuration file
+    file_put_contents($target, $contents);
 });
